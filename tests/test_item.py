3a92315f2377844e1f7b33bbe619c645587b1daf
KibalinMax <kibalaspb@mail.ru>
2023-10-14 17:26:44 +0300

    change5

--- tests/test_item.py
+++ tests/test_item.py
@@ -20,5 +20,4 @@ def test_item_apply_discount():
     assert item_pr_2.price == 10000
 def test_instantiate_from_csv():
     Item.instantiate_from_csv()
-    assert len(Item.all) == 8
-
+    assert len(Item.all) == 5
